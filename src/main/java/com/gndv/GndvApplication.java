2a2e061c809b64c5a29bf8d3a762ac3e2455cef5
12Ahn22 <12ayo22@gmail.com>
2024-05-22 15:32:27 +0900

    init: 첫 프로젝트 생성

---
+++ src/main/java/com/gndv/GndvApplication.java
@@ -0,0 +1,13 @@
+package com.gndv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GndvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GndvApplication.class, args);
+    }
+
+}
