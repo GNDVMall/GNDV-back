a3298fdc3b17c916fec7ba2e4500e999b2575fa2
12Ahn22 <12ayo22@gmail.com>
2024-05-27 14:17:49 +0900

    feat: Item Dto 생성

---
+++ src/main/java/com/gndv/item/domain/entity/Item.java
@@ -0,0 +1,18 @@
+package com.gndv.item.domain.entity;
+
+import java.time.LocalDateTime;
+
+public class Item {
+    private Long item_id;
+    private String item_number;
+    private Long regular_price;
+    private String age_range;
+    private Long pieces;
+    private String item_name;
+    private Long recent_price;
+    private LocalDateTime release_date;
+    private Long wish_count;
+    private Long view_count;
+    private Long theme_id;
+    private Long recommend;
+}
