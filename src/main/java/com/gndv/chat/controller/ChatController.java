1f790ea1a2d610866eae1b90b129e8652a1c9cdc
12Ahn22 <12ayo22@gmail.com>
2024-05-31 09:33:55 +0900

    refactor : 채팅룸 생성 api 반환값 타입 변경

--- src/main/java/com/gndv/chat/controller/ChatController.java
+++ src/main/java/com/gndv/chat/controller/ChatController.java
@@ -6,10 +6,7 @@ import com.gndv.common.CustomResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -20,7 +17,7 @@ public class ChatController {
 
     @PostMapping("")
     @PreAuthorize("#chatRoomCreateRequest.email == authentication.name")
-    public CustomResponse<Object> createRoom(@RequestBody ChatRoomCreateRequest chatRoomCreateRequest){
+    public CustomResponse<ChatRoomCreateRequest> createRoom(@RequestBody ChatRoomCreateRequest chatRoomCreateRequest){
         log.info("Create ChatRoom {} ", chatRoomCreateRequest);
         // 채팅방을 만들 때, 채팅방 + 채팅방 유저도 만들어야 한다.
         chatService.createChatRoom(chatRoomCreateRequest);
