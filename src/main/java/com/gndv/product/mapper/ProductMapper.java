60050d9cffdeb7f26932feab51fbe1267e84d2f1
12Ahn22 <12ayo22@gmail.com>
2024-06-05 21:11:57 +0900

    fix: findById쿼리 구문 잘못된 부분 수정

--- src/main/java/com/gndv/product/mapper/ProductMapper.java
+++ src/main/java/com/gndv/product/mapper/ProductMapper.java
@@ -12,11 +12,10 @@ import java.util.Optional;
 
 @Mapper
 public interface ProductMapper {
-    @Select("SELECT p.*, m.nickname , m.introduction, m.rating, m.profile_url, GROUP_CONCAT(i.real_filename) AS images FROM  Product p JOIN Image i ON p.product_id = i.use_id JOIN Member_With_Profile m ON p.member_id = m.member_id WHERE i.image_type = 'product' AND p.product_id = #{product_id} GROUP BY p.product_id")
+    @Select("SELECT p.*, m.nickname, m.introduction, m.rating, m.profile_url, GROUP_CONCAT(i.real_filename) AS images \n" +
+            "FROM Product p LEFT JOIN Image i ON p.product_id = i.use_id LEFT JOIN Member_With_Profile m ON p.member_id = m.member_id \n" +
+            "WHERE p.product_id = #{product_id} GROUP BY p.product_id")
     Optional<ProductDetailResponse> findById(Long product_id);
-
-//    @Select("SELECT p.* , GROUP_CONCAT(i.real_filename) AS images FROM  Product p LEFT JOIN Image i ON p.product_id = i.use_id GROUP BY p.product_id HAVING p.item_id = #{item_id}")
-//    List<ProductResponse> findAllById(Long item_id, PagingRequest pagingRequest);
 
     @Select("SELECT p.* , GROUP_CONCAT(i.real_filename) AS images, (SELECT COUNT(*) FROM Product WHERE item_id = #{item_id}) AS total" +
             " FROM  Product p LEFT JOIN Image i ON p.product_id = i.use_id GROUP BY p.product_id HAVING p.item_id = #{item_id} order by p.created_at desc LIMIT #{skip}, #{size}")
