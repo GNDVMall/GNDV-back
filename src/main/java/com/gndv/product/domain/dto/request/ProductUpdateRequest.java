3d7666f8e4f08c75f76d93c20e76e596476a95ee
12Ahn22 <44540726+12Ahn22@users.noreply.github.com>
2024-06-13 15:14:08 +0900

    Feat: 프로덕트 수정, 생성 시 이미지 처리 기능 추가 (#73)
    * fix: 누락된 시큐리티 코드 추가
    
    * feat: 채팅방의 채팅 메시지를 가져오는 api
    - 해당 채팅방 메시지 목록을 전부 가져옴
    - 내가 쓰지않은 메시지는 가져온 후에, 모두 읽음처리 (저장 프로시저 사용)
    
    * feat: 웹소켓으로 통신하는 기능 추가
    - 해당 방으로 소켓 연결을 하고 채팅을 주고받을 수 있음
    
    * feat: 채팅 시, 채팅 메시지를 DB에 저장하는 로직 추가
    - 일단은 채팅을 칠 때마다 DB와 연결
    
    * style: 주석, 콘솔 코드 삭제
    
    * chore: 웹소켓 의존성 추가
    
    * feat: 웹 소켓 설정
    
    * feat: 이전에 작업하던 chatting 브랜치와 병합
    
    * feat: 웹 소켓용 JWT 검증 & 유저 정보 세션에 저장
    
    * feat: 웹 소켓용 JWT 검증 & 유저 정보 세션에 저장
    
    * feat: 로그인 시, 이메일 정보를 반환하도록 설정
    
    * feat: 채팅메시지 생성 시, 메시지 id를 반환받아 사용할 수 있도록 변경
    
    * chore: 파일명, 클래스명 다른 오타 수정
    
    * feat: Products 수정 시,값이 있는 부분만 변경하도록 SQL 변경
    
    * style: ProductSalesStatus 오타 수정
    
    * feat: 채팅방 정보를 가져올 때, 제품 정보도 함께 가져오기
    
    * feat: 채팅 알림용 구독 추가
    
    * feat: 채팅방 리스트 api에 안읽은 채팅 정보를 추가
    
    * refactor: 채팅방 나가는 로직 변경
    - Delete가 아니라 leave라는 상태를 업데이트
    
    * refactor: 채팅방 나가는 로직 변경으로 채팅방 목록을 가져오는 쿼리 변경
    
    * refactor: 시스템 메시지를 위한 채팅 유저 타입 추가 + 방 떠남 처리를 위한 일부 변경사항
    
    * feat: 프로덕트 생성 후, id 값을 반환하도록 변경
    
    * fix: 웹소켓 라이브러리가 빠진 오류 수정
    
    * feat: 이미지를 위한 프로덕트 컨트롤러 수정중
    
    * feat: 상품 등록 시, 이미지도 등록되는 기능 추가
    
    * fix: 프로덕트 이미지 업로드 경로 폴더명 변경
    
    * feat: 프로덕트 수정 시, 이미지 변경 및 삭제
    
    * fix: 빈 값을 보내는 경우 예외처리

--- src/main/java/com/gndv/product/domain/dto/request/ProductUpdateRequest.java
+++ src/main/java/com/gndv/product/domain/dto/request/ProductUpdateRequest.java
@@ -3,11 +3,14 @@ package com.gndv.product.domain.dto.request;
 import com.gndv.constant.Boolean;
 import com.gndv.constant.ProductSalesStatus;
 import com.gndv.constant.ProductStatus;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
-@Getter
-@Setter
+import java.util.List;
+
+@Data
 public class ProductUpdateRequest {
     private String email;
     private Long product_id;
@@ -18,4 +21,6 @@ public class ProductUpdateRequest {
     private Boolean product_trade_opt1;
     private Boolean product_trade_opt2;
     private ProductSalesStatus product_sales_status;
+    private List<MultipartFile> images;
+    private List<String> delete_images;
 }
