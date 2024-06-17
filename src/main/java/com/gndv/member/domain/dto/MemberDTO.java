becb9710bd7a2f489d62cfe59449bf0bf51c8829
imhungry2 <sjnnj@naver.com>
2024-06-17 15:38:03 +0900

    feat(Member): 직렬화

--- src/main/java/com/gndv/member/domain/dto/MemberDTO.java
+++ src/main/java/com/gndv/member/domain/dto/MemberDTO.java
@@ -7,13 +7,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class MemberDTO {
+public class MemberDTO implements Serializable {
 
     // Member
     private Long member_id;
