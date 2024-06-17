9e3a1e3d810517e88e8c5f7ccc9c121f72822840
bizbaeja <bjgbjg112@gmail.com>
2024-06-17 10:37:13 +0900

    fix:recentProduct

--- src/main/java/com/gndv/product/controller/RecentProductController.java
+++ src/main/java/com/gndv/product/controller/RecentProductController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/recent-product")
+@RequestMapping("/api/v2/recent-product")
 @RequiredArgsConstructor
 @Slf4j
 public class RecentProductController {
