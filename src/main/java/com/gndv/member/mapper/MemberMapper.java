57493fc46dcf84af0ee330fd51fd96d056571378
CopyPaste <139524617+imhungry2@users.noreply.github.com>
2024-06-18 10:32:09 +0900

    Feat/member imhungry2 (#104)
    * fix(Security): 세션의 저장되는 객체 수정
    
    * style(Security): @EnableMethodSecurity를 이용하여 접근 권한을 설정하기 위해 모든 경로에 대한 permitAll() 처리
    
    * style(Member): @EnableMethodSecurity 적용하여 접근 권한을 확인할 수 있도록 코드 수정
    
    * style(Security): 코드 수정
    
    * style(Security): 코드 수정
    
    * feat(JWT): 의존성 추가
    
    * feat(Member): refreshToken 추가
    
    * feat(Member): access = AccessLevel.PROTECTED
    
    * feat(Member): findByRefreshToken 추가
    
    * feat(token): jwt 관련 설정
    
    * feat(token): jwt 필터를 사용하여 로그인 시 헤더에 토큰 값 생성완료
    
    * style(token): 코드 정리
    
    * refactor(token): 코드 리팩토링
    
    * style: 코드 스타일 수정
    
    * feat(oauth2): 의존성 추가
    
    * style: 스타일 변경
    
    * style: 스타일 변경
    
    * feat(oauth2): 소셜 로그인 설정
    
    * fix(JWTToken): 토큰 필터 버그 수정
    
    * fix(JWTToken): 토큰 필터 버그 수정
    
    * style
    
    * feat(oauth2): 의존성 추가
    
    * feat(oauth2): 의존성 추가
    
    * feat(oauth2): 카카오 로그인 추가
    
    * feat(oauth2): DefaultOAuth2UserService 상속
    
    * style(Member): 코드 수정
    
    * style: 코드 수정
    
    * style: 코드 수정
    
    * style: 코드 수정
    
    * style: 코드 수정
    
    * feat(Member): Profile 작업
    
    * feat(Member): Profile 이미지 작업완료
    
    * feat(Member): JoinRequest 이메일 Valid 추가
    
    * feat(Member): 이메일 인증 의존성 추가
    
    * feat(Member): EmailConfig 추가
    
    * style(Member): 코드 수정
    
    * style(Member): 코드 수정
    
    * feat: 이메일 인증을 위한 redis와 이메일 의존성 추가
    
    * feat: 이메일 인증을 위한 작업
    
    * feat(Email): redis를 활용한 이메일 인증 작업완료
    
    * style(Member)
    
    * style(Member): coolsms 의존성 추가
    
    * feat(Member): 휴대폰 인증 후 회원 등급 SELLER로 변경
    
    * feat(Member): 환경변수 수정
    
    * feat(Member): 프로필 작업
    
    * fix(Member): 마이바티스 프로필 타입 디코더 문제 해결
    
    * fix(Member): 마이바티스 프로필 타입 디코더 문제 해결
    
    * fix(Member): 프로필 비밀번호 픽스
    
    * fix(Profile): 회원 프로필 추가 정보
    
    * style
    
    * fix(Member): 프로필 페이징과 버그 해결
    
    * feat(Member): 프로필 페이지 마지막 로그인 시간 추가 및 로그인마다 시간 업데이트
    
    * fix(Security): 시큐리티 메소드 버그로 인한 임시 해제
    
    * fix(Member): MemberProfile 리뷰 리스트 버그 수정
    
    * style(Member)
    
    * feat(Member): 직렬화
    
    * feat(Member): validation

--- src/main/java/com/gndv/member/mapper/MemberMapper.java
+++ src/main/java/com/gndv/member/mapper/MemberMapper.java
@@ -13,6 +13,10 @@ import java.util.Optional;
 @Mapper
 public interface MemberMapper {
 
+    @Insert("INSERT INTO Member (email, password) VALUES (#{email}, #{password})")
+    @Transactional
+    void insert(Member member);
+
     @Select("SELECT * FROM Member WHERE member_id = #{member_id}")
     Optional<Member> findById(Long member_id);
 
@@ -26,12 +30,14 @@ public interface MemberMapper {
             "FROM Member WHERE email = #{email}")
     Optional<Member> getMemberProfile(String email);
 
-    @Select("SELECT * FROM member")
-    List<Member> findAll();
+    @Select("SELECT * FROM MemberProfileView WHERE email = #{email} LIMIT #{limit} OFFSET #{offset}")
+    List<ProfileDetailsRequest> getMemberProfileDetails(String email, int offset, int limit);
 
-    @Insert("INSERT INTO Member (email, password) VALUES (#{email}, #{password})")
-    @Transactional
-    void insert(Member member);
+    @Select("SELECT COUNT(*) FROM MemberProfileView WHERE email = #{email}")
+    int countReviewsByEmail(String email);
+
+    @Select("SELECT * FROM Member WHERE refreshToke = #{refreshToke}")
+    Optional<Member> findByRefreshToken(String refreshToke);
 
     @Update({
             "<script>",
@@ -56,25 +62,16 @@ public interface MemberMapper {
     @Update("UPDATE Member SET profile_url = #{profile_url} WHERE member_id = #{member_id}")
     void updateProfileImage(Long member_id, String profile_url);
 
-    @Delete("DELETE FROM Member WHERE member_id = #{member_id}")
-    @Transactional
-    void delete(Long member_id);
+    @Update("UPDATE Member SET role = #{role} WHERE member_id = #{member_id}")
+    void updateSeller(Long member_id, Role role);
 
-    @Select("SELECT * FROM Member WHERE refreshToke = #{refreshToke}")
-    Optional<Member> findByRefreshToken(String refreshToke);
+    @Update("UPDATE Member SET last_login = #{last_login} WHERE member_id = #{member_id}")
+    void updateLastLogin(Long member_id, Date last_login);
 
     @Update("UPDATE Member SET accessToken = #{accessToken}, refreshToken = #{refreshToken} WHERE member_id = #{member_id}")
     void updateTokens(Long member_id, String accessToken, String refreshToken);
 
-    @Update("UPDATE Member SET role = #{role} WHERE member_id = #{member_id}")
-    void updateSeller(Long member_id, Role role);
-
-    @Select("SELECT * FROM MemberProfileView WHERE email = #{email} LIMIT #{limit} OFFSET #{offset}")
-    List<ProfileDetailsRequest> getMemberProfileDetails(String email, int offset, int limit);
-
-    @Select("SELECT COUNT(*) FROM MemberProfileView WHERE email = #{email}")
-    int countReviewsByEmail(String email);
-
-    @Update("UPDATE Member SET last_login = #{last_login} WHERE member_id = #{member_id}")
-    void updateLastLogin(Long member_id, Date last_login);
+    @Delete("DELETE FROM Member WHERE member_id = #{member_id}")
+    @Transactional
+    void delete(Long member_id);
 }
