5367d7e3abed23399b1c769dbb6de9630dc253f5
imhungry2 <sjnnj@naver.com>
2024-06-18 17:27:18 +0900

    EqualsAndHashCode

--- src/main/java/com/gndv/product/domain/dto/request/ProductListPagingRequest.java
+++ src/main/java/com/gndv/product/domain/dto/request/ProductListPagingRequest.java
@@ -2,7 +2,9 @@ package com.gndv.product.domain.dto.request;
 
 import com.gndv.common.domain.request.PagingRequest;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class ProductListPagingRequest extends PagingRequest {
     private Long item_id;
